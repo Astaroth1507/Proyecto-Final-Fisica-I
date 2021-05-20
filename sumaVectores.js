88f7e84b352ba05de5360f28b25cceaa109a7d4c
Astaroth1507 <monterrosoalexander70@gmail.com>
2021-05-19 20:31:08 -0600

    Suma de vectores termindada

--- sumaVectores.js
+++ sumaVectores.js
@@ -12,41 +12,102 @@ const cuadranteVectorTres = document.getElementById("cuadranteV3")
 const cuadranteVectorCuatro = document.getElementById("cuadranteV4")
 const calcular = document.getElementById("calcular")
 const limpiar = document.getElementById("limpiar")
-const toRadians = rad => rad * (Math.PI / 180)
+const toRadians = rad => rad * Math.PI / 180
+const componentesX = []
+const componentesY = []
+
 
 const descomposicionVPX = (magnitud, angulo, cuadrante) => {
-    let resultadosEnX = (magnitud * (Math.cos(toRadians(angulo)))).toFixed(3)
+    let resultadosEnX = (magnitud * (Math.cos(toRadians(angulo)))).toFixed(2)
     if (cuadrante == 2 || cuadrante == 3) {
         return resultadosEnX * -1
+    } else if (cuadrante == 5 || cuadrante == 7) {
+        return resultadosEnX * 0
+    } else if (cuadrante == 6) {
+        return magnitud;
+    } else if (cuadrante == 8) {
+        return magnitud * -1
+    } else {
+        return resultadosEnX
     }
-    return resultadosEnX
-
 }
 
 const descomposicionVPY = (magnitud, angulo, cuadrante) => {
-    let resultadosEnY = (magnitud * (Math.sin(toRadians(angulo)))).toFixed(3)
+    let resultadosEnY = (magnitud * (Math.sin(toRadians(angulo)))).toFixed(2)
     if (cuadrante == 3 || cuadrante == 4) {
         return resultadosEnY * -1
+    } else if (cuadrante == 6 || cuadrante == 8) {
+        return resultadosEnY * 0
+    } else if (cuadrante == 5) {
+        return magnitud
+    } else if (cuadrante == 7) {
+        return magnitud * -1
+    } else {
+        return resultadosEnY
     }
-    return resultadosEnY
+}
+const sumaVectores = (vectoresEnX, vectoresEnY) => {
+
 }
 
-
 $(calcular).click(function() {
-    console.log("ESTE DE AQUI ES EL VECTOR 1");
-    console.log(descomposicionVPX(vPmagnitud.value, vPgrado.value, cuadranteVectorUno.value));
-    console.log("-------------------------------------------------------------------");
-    console.log(descomposicionVPY(vPmagnitud.value, vPgrado.value, cuadranteVectorUno.value));
-    console.log("ESTE DE AQUI ES EL VECTOR 2");
-    console.log(descomposicionVPX(vSmagnitud.value, vSgrado.value, cuadranteVectorDos.value));
-    console.log("-------------------------------------------------------------------");
-    console.log(descomposicionVPY(vSmagnitud.value, vSgrado.value, cuadranteVectorDos.value));
-    console.log("ESTE DE AQUI ES EL VECTOR 3");
-    console.log(descomposicionVPX(vTmagnitud.value, vTgrado.value, cuadranteVectorTres.value));
-    console.log("-------------------------------------------------------------------");
-    console.log(descomposicionVPY(vTmagnitud.value, vTgrado.value, cuadranteVectorTres.value));
-    console.log("ESTE DE AQUI ES EL VECTOR 4");
-    console.log(descomposicionVPX(vCmagnitud.value, vCgrado.value, cuadranteVectorCuatro.value));
-    console.log("-------------------------------------------------------------------");
-    console.log(descomposicionVPY(vCmagnitud.value, vCgrado.value, cuadranteVectorCuatro.value))
+    console.log("VECTORES EN X");
+    componentesX.push(parseFloat(descomposicionVPX(vPmagnitud.value, vPgrado.value, cuadranteVectorUno.value)),
+        parseFloat(descomposicionVPX(vSmagnitud.value, vSgrado.value, cuadranteVectorDos.value)),
+        parseFloat(descomposicionVPX(vTmagnitud.value, vTgrado.value, cuadranteVectorTres.value)),
+        parseFloat(descomposicionVPX(vCmagnitud.value, vCgrado.value, cuadranteVectorCuatro.value)))
+    console.table(componentesX)
+    console.log("VECTORES EN Y");
+    componentesY.push(parseFloat(descomposicionVPY(vPmagnitud.value, vPgrado.value, cuadranteVectorUno.value)),
+        parseFloat(descomposicionVPY(vSmagnitud.value, vSgrado.value, cuadranteVectorDos.value)),
+        parseFloat(descomposicionVPY(vTmagnitud.value, vTgrado.value, cuadranteVectorTres.value)),
+        parseFloat(descomposicionVPY(vCmagnitud.value, vCgrado.value, cuadranteVectorCuatro.value)))
+    let sumaX = 0
+    let sumaY = 0
+    componentesX.forEach(element => {
+        sumaX += element
+    })
+    componentesY.forEach(element => {
+        sumaY += element
+    })
+    let vectorResultante = Math.sqrt((sumaX * sumaX) + (sumaY * sumaY)).toFixed(2)
+    let anguloResultante = Math.atan(sumaY / sumaX) * (180 / Math.PI)
+    console.log(anguloResultante)
+
+    function anguloSHorizontal() {
+        if (anguloResultante < 0 && anguloResultante > -90) {
+            return (anguloResultante) + 180
+        } else if (anguloResultante > 0 && anguloResultante < 90) {
+            return (anguloResultante) + 90
+        } else if (anguloResultante > -90 && anguloResultante > -180) {
+            return (anguloResultante) + 270
+        } else {
+            return (anguloResultante) + 360
+        }
+    }
+    let anguloHorizontal = anguloSHorizontal()
+    formulasDVectore()
+    formulaYresultante(vectorResultante)
+    respuestas(vectorResultante, anguloHorizontal.toFixed(2))
 })
+
+function formulasDVectore() {
+    $("#descomposicionVectoresXY").html(
+        `<img src="https://latex.codecogs.com/svg.image?x=cos\\theta&space;(h)" title="x=cos\\theta (h)">
+        <img src="https://latex.codecogs.com/svg.image?y=sin\\theta&space;(h)" title="y=sin\\theta (h)">`
+    )
+}
+
+function formulaYresultante() {
+    $("#formulaResultantes").html(
+        `<img src="https://latex.codecogs.com/svg.image?R=&space;\\sqrt[2]{x^{2}&plus;y^{2}}" title="R= \\sqrt[2]{x^{2}+y^{2}}">
+
+        <img src="https://latex.codecogs.com/svg.image?\\theta&space;=&space;tan^{-1}(\\frac{y}{x})" title="\\theta = tan^{-1}(\\frac{y}{x})">`
+    )
+}
+
+function respuestas(resultante, angulo) {
+    $("#respuestas").html(
+        `<span>R//${resultante}, ${angulo}</span>`
+    )
+}
