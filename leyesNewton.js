556c103c4916d2349669dae972234cd4cc21c8c5
Astaroth1507 <monterrosoalexander70@gmail.com>
2021-05-21 12:12:45 -0600

    masa a peso

--- leyesNewton.js
+++ leyesNewton.js
@@ -1,31 +1,47 @@
 const pesoW = document.getElementById("peso")
 const anguloG = document.getElementById("angulo")
 const opciones = document.getElementById("opciones")
+const masaG = document.getElementById("masa")
 const toRadians = rad => rad * Math.PI / 180
 
 
 
-$('#limpiar').click(function() {})
 
 $(calcular).click(function() {
-    const calcTension = (peso, angulo, opcion) => {
+    const calcTension = (peso, angulo, masa, opcion) => {
         let resultado = 0
         if (opcion == 1 || opcion == 4) {
-            angulo = 90 - angulo
-            resultado = (peso * Math.cos(toRadians(angulo)))
-            console.log(resultado)
-            return resultado
+            if (masa > 0) {
+                angulo = 90 - angulo
+                resultado = masa * 9.8 * Math.cos(toRadians(angulo))
+                return resultado
+            } else {
+                angulo = 90 - angulo
+                resultado = (peso * Math.cos(toRadians(angulo)))
+                console.log(resultado)
+                return resultado
+            }
         } else if (opcion == 2) {
-            resultado = peso / Math.sin(toRadians(angulo))
-            console.log(resultado)
-            return resultado
+            if (masa > 0) {
+                resultado = (masa * 9.8) / Math.sin(toRadians(angulo))
+                return resultado
+            } else {
+                resultado = peso / Math.sin(toRadians(angulo))
+                console.log(resultado)
+                return resultado
+            }
         } else if (opcion == 3) {
-            resultado = peso / Math.sin(toRadians(angulo)) * Math.cos(toRadians(angulo))
-            console.log(resultado)
-            return resultado
+            if (masa > 0) {
+                resultado = (masa * 9.8) / Math.sin(toRadians(angulo)) * Math.cos(toRadians(angulo))
+                return resultado
+            } else {
+                resultado = peso / Math.sin(toRadians(angulo)) * Math.cos(toRadians(angulo))
+                console.log(resultado)
+                return resultado
+            }
         }
     }
-    let results = calcTension(pesoW.value, anguloG.value, opciones.value).toFixed(3)
+    let results = calcTension(pesoW.value, anguloG.value, masaG.value, opciones.value).toFixed(3)
     respuestas(results)
 })
 
